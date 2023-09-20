0511d6485cd3814e07a91019cb32810f138484fb
Надежда Белова <belovanadezhda1983@gmail.com>
2023-09-20 21:58:20 +0300

    register class MyUserAdmin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,15 +1,37 @@
-from django.contrib import admin
-from .models import MyUser
-from recipes.models import (
-    Tag,
-    Ingredient,
-    # RecipeIngredient,
-    Recipe
-)
+from django.contrib.admin import register
+from django.contrib.auth.admin import UserAdmin
+from users.models import MyUser
 
 
-admin.site.register(MyUser)
-admin.site.register(Tag)
-admin.site.register(Ingredient)
-# admin.site.register(RecipeIngredient)
-admin.site.register(Recipe)
+@register(MyUser)
+class MyUserAdmin(UserAdmin):
+    list_display = (
+        "is_active",
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+    )
+    fields = (
+        ("is_active",),
+        (
+            "username",
+            "email",
+        ),
+        (
+            "first_name",
+            "last_name",
+        ),
+    )
+    fieldsets = []
+
+    search_fields = (
+        "username",
+        "email",
+    )
+    list_filter = (
+        "is_active",
+        "first_name",
+        "email",
+    )
+    save_on_top = True
