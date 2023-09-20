c4c000e07c56742cecc0d7bf57b6e81e19bbf25e
Надежда Белова <belovanadezhda1983@gmail.com>
2023-09-20 22:07:09 +0300

    add delete_image signal

---
+++ backend/core/signals.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from recipes.models import Recipe
+
+
+@receiver(post_delete, sender=Recipe)
+def delete_image(sender: Recipe, instance: Recipe, *a, **kw) -> None:
+    """
+    """
+    image = Path(instance.image.path)
+    if image.exists():
+        image.unlink()
