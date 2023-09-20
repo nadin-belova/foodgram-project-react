d645beaadf00bbd12a9b7417d6167e9bf86b7354
Надежда Белова <belovanadezhda1983@gmail.com>
2023-09-20 21:54:08 +0300

    add tags, ingredients, users and auth endpoints

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,18 +1,30 @@
 from api.views import (
+    BaseAPIRootView,
+    IngredientViewSet,
     RecipeViewSet,
+    TagViewSet,
+    UserViewSet,
 )
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-
 app_name = "api"
 
 
-router = DefaultRouter(trailing_slash=False)
+class RuDefaultRouter(DefaultRouter):
+    """
+    """
 
+    APIRootView = BaseAPIRootView
+
+
+router = RuDefaultRouter()
+router.register("tags", TagViewSet, "tags")
+router.register("ingredients", IngredientViewSet, "ingredients")
 router.register("recipes", RecipeViewSet, "recipes")
-
+router.register("users", UserViewSet, "users")
 
 urlpatterns = (
     path("", include(router.urls)),
+    path("auth/", include("djoser.urls.authtoken")),
 )
