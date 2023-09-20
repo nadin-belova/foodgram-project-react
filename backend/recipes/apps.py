ca8c50db26453298e663ebcc1e073b2d83645c82
Надежда Белова <belovanadezhda1983@gmail.com>
2023-09-20 21:59:55 +0300

    add verbose_name and ready func

--- backend/recipes/apps.py
+++ backend/recipes/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class RecipesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipes'
+    verbose_name = 'Рецепты'
+
+    def ready(self) -> None:
+        from core import signals
