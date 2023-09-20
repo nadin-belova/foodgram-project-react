4fd3f0291957f2f6537c8eed56ed678c8e46d4ba
Надежда Белова <belovanadezhda1983@gmail.com>
2023-09-20 21:47:57 +0300

    recreate models with new db schema

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,128 +1,305 @@
+from core.enums import Limits, Tuples
+from core.validators import OneOfTwoValidator, hex_color_validator
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db.models import (
-    Model,
+    CASCADE,
+    SET_NULL,
     CharField,
-    ImageField,
+    CheckConstraint,
+    DateTimeField,
     ForeignKey,
-    # PROTECT,
-    DO_NOTHING,
-    IntegerField,
-    TextField,
+    ImageField,
     ManyToManyField,
+    Model,
+    PositiveSmallIntegerField,
+    Q,
+    TextField,
+    UniqueConstraint,
 )
-# from django.contrib.contenttypes.models import ContentType
-# from users.models import MyUser
+from django.db.models.functions import Length
+from PIL import Image
 
+CharField.register_lookup(Length)
 
-MAX_LEN_TAG_NAME_CHARFIELD = 50
-MAX_LEN_TAG_COLOR_CHARFIELD = 7
-MAX_LEN_TAG_SLUG_CHARFIELD = 20
-MAX_LEN_INGREDIENT_CHARFIELD = 100
-MAX_LEN_UNIT_CHARFIELD = 25
-MAX_LEN_RECIPE_CHARFIELD = 100
-MAX_LEN_PICTURE_CHARFIELD = 255
+User = get_user_model()
 
-
-# models.CASCADE
-# models.PROTECT — запрещает удалять пользователя, пока у него есть посты.
-# models.SET_NULL — посты останутся в БД даже при удалении автора,
-# но значение в поле author у постов изменится на None.
-# models.SET_DEFAULT
 
 class Tag(Model):
+    """Тэги"""
+
     name = CharField(
-        verbose_name="название тэга",
-        max_length=MAX_LEN_TAG_NAME_CHARFIELD
+        verbose_name="Тэг",
+        max_length=Limits.MAX_LEN_RECIPES_CHARFIELD.value,
+        validators=(OneOfTwoValidator(field="Название тэга"),),
+        unique=True,
     )
     color = CharField(
-        verbose_name="цвет тэга",
-        max_length=MAX_LEN_TAG_COLOR_CHARFIELD
+        verbose_name="Цвет",
+        max_length=7,
+        unique=True,
+        db_index=False,
     )
     slug = CharField(
-        verbose_name="слаг тэга",
-        max_length=MAX_LEN_TAG_SLUG_CHARFIELD
+        verbose_name="Слаг тэга",
+        max_length=Limits.MAX_LEN_RECIPES_CHARFIELD.value,
+        unique=True,
+        db_index=False,
     )
 
     class Meta:
         verbose_name = "Тэг"
         verbose_name_plural = "Тэги"
+        ordering = ("name",)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return f"{self.name} (цвет: {self.color})"
+
+    def clean(self) -> None:
+        self.name = self.name.strip().lower()
+        self.slug = self.slug.strip().lower()
+        self.color = hex_color_validator(self.color)
+        return super().clean()
 
 
 class Ingredient(Model):
+    """Ингридиенты"""
+
     name = CharField(
-        verbose_name="Ингредиент",
-        max_length=MAX_LEN_INGREDIENT_CHARFIELD,
+        verbose_name="Ингридиент",
+        max_length=Limits.MAX_LEN_RECIPES_CHARFIELD.value,
     )
     measurement_unit = CharField(
-        verbose_name="Единица измерения",
-        max_length=MAX_LEN_UNIT_CHARFIELD,
-    )
-    amount = IntegerField(
-        verbose_name="количество",
-        null=False,
+        verbose_name="Единицы измерения",
+        max_length=24,
     )
 
     class Meta:
-        verbose_name = "Ингредиент"
-        verbose_name_plural = "Ингредиенты"
+        verbose_name = "Ингридиент"
+        verbose_name_plural = "Ингридиенты"
+        ordering = ("name",)
+        constraints = (
+            UniqueConstraint(
+                fields=("name", "measurement_unit"),
+                name="unique_for_ingredient",
+            ),
+            CheckConstraint(
+                check=Q(name__length__gt=0),
+                name="\n%(app_label)s_%(class)s_name is empty\n",
+            ),
+            CheckConstraint(
+                check=Q(measurement_unit__length__gt=0),
+                name="\n%(app_label)s_%(class)s_measurement_unit is empty\n",
+            ),
+        )
 
-    def __str__(self):
-        return f'{self.name}, {self.amount} {self.measurement_unit}'
+    def __str__(self) -> str:
+        return f"{self.name} {self.measurement_unit}"
 
-
-# class RecipeIngredient(Model):
-#     ingredient = ForeignKey(
-#         verbose_name="ингредиенты рецепта",
-#         to=Ingredient,
-#         on_delete=PROTECT,
-#         null=False,
-#     )
-    # class Meta:
-    #     verbose_name = "Ингредиенты рецепта"
-    #     verbose_name_plural = "Ингредиенты рецептов"
-
-    # def __str__(self):
-    #     return (
-    #         self.ingredient.name + ', ' +
-    #         self.quantity.__str__()
-    #     )
+    def clean(self) -> None:
+        self.name = self.name.lower()
+        self.measurement_unit = self.measurement_unit.lower()
+        super().clean()
 
 
 class Recipe(Model):
-    # author = ForeignKey(
-    #     verbose_name="автор",
-    #     to='users.Myuser',
-    #     on_delete=DO_NOTHING
-    # )
+    """Рецепты"""
+
     name = CharField(
-        verbose_name="рецепт",
-        max_length=MAX_LEN_RECIPE_CHARFIELD,
+        verbose_name="Название блюда",
+        max_length=Limits.MAX_LEN_RECIPES_CHARFIELD.value,
     )
-    image = ImageField(
-        verbose_name="картинка",
-        max_length=MAX_LEN_PICTURE_CHARFIELD,
-        blank=True
-    )
-    description = TextField(
-        verbose_name="описание",
-    )
-    ingredients = ManyToManyField(
-        Ingredient,
-        verbose_name="ингредиент",
+    author = ForeignKey(
+        verbose_name="Автор рецепта",
+        related_name="recipes",
+        to=User,
+        on_delete=SET_NULL,
+        null=True,
     )
     tags = ManyToManyField(
-        Tag,
-        verbose_name="тэг",
+        verbose_name="Тег",
+        related_name="recipes",
+        to="Tag",
     )
-    cooking_time = IntegerField(
-        verbose_name="время приготовления",
+    ingredients = ManyToManyField(
+        verbose_name="Ингредиенты блюда",
+        related_name="recipes",
+        to=Ingredient,
+        through="recipes.AmountIngredient",
+    )
+    pub_date = DateTimeField(
+        verbose_name="Дата публикации",
+        auto_now_add=True,
+        editable=False,
+    )
+    image = ImageField(
+        verbose_name="Изображение блюда",
+        upload_to="recipe_images/",
+    )
+    text = TextField(
+        verbose_name="Описание блюда",
+        max_length=Limits.MAX_LEN_RECIPES_TEXTFIELD.value,
+    )
+    cooking_time = PositiveSmallIntegerField(
+        verbose_name="Время приготовления",
+        default=0,
+        validators=(
+            MinValueValidator(
+                Limits.MIN_COOKING_TIME.value,
+                "Ваше блюдо уже готово!",
+            ),
+            MaxValueValidator(
+                Limits.MAX_COOKING_TIME.value,
+                "Очень долго ждать...",
+            ),
+        ),
     )
 
     class Meta:
         verbose_name = "Рецепт"
         verbose_name_plural = "Рецепты"
+        ordering = ("-pub_date",)
+        constraints = (
+            UniqueConstraint(
+                fields=("name", "author"),
+                name="unique_for_author",
+            ),
+            CheckConstraint(
+                check=Q(name__length__gt=0),
+                name="\n%(app_label)s_%(class)s_name is empty\n",
+            ),
+        )
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return f"{self.name}. Автор: {self.author.username}"
+
+    def clean(self) -> None:
+        self.name = self.name.capitalize()
+        return super().clean()
+
+    def save(self, *args, **kwargs) -> None:
+        super().save(*args, **kwargs)
+        image = Image.open(self.image.path)
+        image.thumbnail(Tuples.RECIPE_IMAGE_SIZE)
+        image.save(self.image.path)
+
+
+class AmountIngredient(Model):
+    """
+    """
+
+    recipe = ForeignKey(
+        verbose_name="В каких рецептах",
+        related_name="ingredient",
+        to=Recipe,
+        on_delete=CASCADE,
+    )
+    ingredients = ForeignKey(
+        verbose_name="Связанные ингредиенты",
+        related_name="recipe",
+        to=Ingredient,
+        on_delete=CASCADE,
+    )
+    amount = PositiveSmallIntegerField(
+        verbose_name="Количество",
+        default=0,
+        validators=(
+            MinValueValidator(
+                Limits.MIN_AMOUNT_INGREDIENTS,
+                "Нужно хоть какое-то количество.",
+            ),
+            MaxValueValidator(
+                Limits.MAX_AMOUNT_INGREDIENTS,
+                "Слишком много!",
+            ),
+        ),
+    )
+
+    class Meta:
+        verbose_name = "Ингридиент"
+        verbose_name_plural = "Количество ингридиентов"
+        ordering = ("recipe",)
+        constraints = (
+            UniqueConstraint(
+                fields=(
+                    "recipe",
+                    "ingredients",
+                ),
+                name="\n%(app_label)s_%(class)s ingredient alredy added\n",
+            ),
+        )
+
+    def __str__(self) -> str:
+        return f"{self.amount} {self.ingredients}"
+
+
+class Favorites(Model):
+    """Избранные рецепты"""
+
+    recipe = ForeignKey(
+        verbose_name="Понравившиеся рецепты",
+        related_name="in_favorites",
+        to=Recipe,
+        on_delete=CASCADE,
+    )
+    user = ForeignKey(
+        verbose_name="Пользователь",
+        related_name="favorites",
+        to=User,
+        on_delete=CASCADE,
+    )
+    date_added = DateTimeField(
+        verbose_name="Дата добавления", auto_now_add=True, editable=False
+    )
+
+    class Meta:
+        verbose_name = "Избранный рецепт"
+        verbose_name_plural = "Избранные рецепты"
+        constraints = (
+            UniqueConstraint(
+                fields=(
+                    "recipe",
+                    "user",
+                ),
+                name="\n%(app_label)s_%(class)s recipe is favorite alredy\n",
+            ),
+        )
+
+    def __str__(self) -> str:
+        return f"{self.user} -> {self.recipe}"
+
+
+class Carts(Model):
+    """Рецепты в корзине"""
+
+    recipe = ForeignKey(
+        verbose_name="Рецепты в списке покупок",
+        related_name="in_carts",
+        to=Recipe,
+        on_delete=CASCADE,
+    )
+    user = ForeignKey(
+        verbose_name="Владелец списка",
+        related_name="carts",
+        to=User,
+        on_delete=CASCADE,
+    )
+    date_added = DateTimeField(
+        verbose_name="Дата добавления", auto_now_add=True, editable=False
+    )
+
+    class Meta:
+        verbose_name = "Рецепт в списке покупок"
+        verbose_name_plural = "Рецепты в списке покупок"
+        constraints = (
+            UniqueConstraint(
+                fields=(
+                    "recipe",
+                    "user",
+                ),
+                name="\n%(app_label)s_%(class)s recipe is cart alredy\n",
+            ),
+        )
+
+    def __str__(self) -> str:
+        return f"{self.user} -> {self.recipe}"
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,48 +1,175 @@
-from recipes.models import Recipe
-from django.db.models import (
-    ForeignKey,
-    SET_NULL
-)
+import unicodedata
+
+from core import texsts
+from core.enums import Limits
+from core.validators import MinLenValidator, OneOfTwoValidator
 from django.contrib.auth.models import AbstractUser
+from django.db.models import (
+    CASCADE,
+    BooleanField,
+    CharField,
+    CheckConstraint,
+    DateTimeField,
+    EmailField,
+    F,
+    ForeignKey,
+    Model,
+    Q,
+    UniqueConstraint,
+)
+from django.db.models.functions import Length
+from django.utils.translation import gettext_lazy as _
+
+CharField.register_lookup(Length)
 
 
 class MyUser(AbstractUser):
-    # subscriptions = ForeignKey(
-    #     verbose_name="подписки",
-    #     to='self',  # Сам на себя
-    #     on_delete=SET_NULL,
-    #     null=True,
-    #     blank=True
-    # )
-    favorite_recipes = ForeignKey(
-        verbose_name="любимый рецепт",
-        to=Recipe,
-        related_name='user_favorite_recipes',
-        on_delete=SET_NULL,
-        null=True,
-        blank=True
+    """
+    """
+
+    email = EmailField(
+        verbose_name="Адрес электронной почты",
+        max_length=Limits.MAX_LEN_EMAIL_FIELD.value,
+        unique=True,
+        help_text=texsts.USERS_HELP_EMAIL,
     )
-    cart = ForeignKey(
-        verbose_name="корзина",
-        to=Recipe,
-        related_name='user_cart',
-        on_delete=SET_NULL,
-        null=True,
-        blank=True
+    username = CharField(
+        verbose_name="Уникальный юзернейм",
+        max_length=Limits.MAX_LEN_USERS_CHARFIELD.value,
+        unique=True,
+        help_text=texsts.USERS_HELP_UNAME,
+        validators=(
+            MinLenValidator(
+                min_len=Limits.MIN_LEN_USERNAME,
+                field="username",
+            ),
+            OneOfTwoValidator(field="username"),
+        ),
     )
-    own_recipe = ForeignKey(
-        verbose_name="мой рецепт",
-        to=Recipe,
-        related_name='author_id',
-        on_delete=SET_NULL,
-        null=True,
-        blank=True
+    first_name = CharField(
+        verbose_name="Имя",
+        max_length=Limits.MAX_LEN_USERS_CHARFIELD.value,
+        help_text=texsts.USERS_HELP_FNAME,
+        validators=(
+            OneOfTwoValidator(
+                first_regex="[^а-яёА-ЯЁ -]+",
+                second_regex="[^a-zA-Z -]+",
+                field="Имя",
+            ),
+        ),
+    )
+    last_name = CharField(
+        verbose_name="Фамилия",
+        max_length=Limits.MAX_LEN_USERS_CHARFIELD.value,
+        help_text=texsts.USERS_HELP_FNAME,
+        validators=(
+            OneOfTwoValidator(
+                first_regex="[^а-яёА-ЯЁ -]+",
+                second_regex="[^a-zA-Z -]+",
+                field="Фамилия",
+            ),
+        ),
+    )
+    password = CharField(
+        verbose_name=_("Пароль"),
+        max_length=128,
+        help_text=texsts.USERS_HELP_FNAME,
+    )
+    is_active = BooleanField(
+        verbose_name="Активирован",
+        default=True,
     )
 
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
         ordering = ("username",)
+        constraints = (
+            CheckConstraint(
+                check=Q(username__length__gte=Limits.MIN_LEN_USERNAME.value),
+                name="\nusername is too short\n",
+            ),
+        )
 
     def __str__(self) -> str:
         return f"{self.username}: {self.email}"
+
+    @classmethod
+    def normalize_email(cls, email: str) -> str:
+        """Normalize the email address by lowercasing the domain part of it."""
+        email = email or ""
+        try:
+            email_name, domain_part = email.strip().rsplit("@", 1)
+        except ValueError:
+            pass
+        else:
+            email = email_name.lower() + "@" + domain_part.lower()
+        return email
+
+    @classmethod
+    def normalize_username(cls, username: str) -> str:
+        return unicodedata.normalize("NFKC", username).capitalize()
+
+    def __normalize_human_names(self, name: str) -> str:
+        """
+        """
+        storage = [None] * len(name)
+        title = True
+        idx = 0
+        for letter in name:
+            letter = letter.lower()
+            if title:
+                if not letter.isalpha():
+                    continue
+                else:
+                    letter = letter.upper()
+                    title = False
+            elif letter in " -":
+                title = True
+            storage[idx] = letter
+            idx += 1
+        return "".join(storage[:idx])
+
+    def clean(self) -> None:
+        self.first_name = self.__normalize_human_names(self.first_name)
+        self.last_name = self.__normalize_human_names(self.last_name)
+        return super().clean()
+
+
+class Subscriptions(Model):
+    """
+    """
+
+    author = ForeignKey(
+        verbose_name="Автор рецепта",
+        related_name="subscribers",
+        to=MyUser,
+        on_delete=CASCADE,
+    )
+    user = ForeignKey(
+        verbose_name="Подписчики",
+        related_name="subscriptions",
+        to=MyUser,
+        on_delete=CASCADE,
+    )
+    date_added = DateTimeField(
+        verbose_name="Дата создания подписки",
+        auto_now_add=True,
+        editable=False,
+    )
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+        constraints = (
+            UniqueConstraint(
+                fields=("author", "user"),
+                name="\nRepeat subscription\n",
+            ),
+            CheckConstraint(
+                check=~Q(author=F("user")), name="\nNo self sibscription\n"
+            ),
+        )
+
+    def __str__(self) -> str:
+        return f"{self.user.username} -> {self.author.username}"
