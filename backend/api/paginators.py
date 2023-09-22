a91ae70e0437b8bd732b59c5c46b134198823338
Надежда Белова <belovanadezhda1983@gmail.com>
2023-09-22 16:39:51 +0300

    докстринг в пагинаторе и page_size = 6

--- backend/api/paginators.py
+++ backend/api/paginators.py
@@ -3,6 +3,9 @@ from rest_framework.pagination import PageNumberPagination
 
 class PageLimitPagination(PageNumberPagination):
     """
+    Класс для настройки пагинации с
+    фиксированным количеством элементов на странице.
     """
 
+    page_size = 6
     page_size_query_param = "limit"
