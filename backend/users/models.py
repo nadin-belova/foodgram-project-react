ace1340abeb592ec62c6e9591aa7d4a3348c1da8
Надежда Белова <belovanadezhda1983@gmail.com>
2023-09-24 17:40:17 +0300

    валидация

--- backend/users/models.py
+++ backend/users/models.py
@@ -97,14 +97,19 @@ class MyUser(AbstractUser):
 
     @classmethod
     def normalize_email(cls, email: str) -> str:
-        """Normalize the email address by lowercasing the domain part of it."""
+        """
+        Нормализация email-адреса путем приведения
+        к нижнему регистру части с доменом.
+        """
         email = email or ""
         try:
             email_name, domain_part = email.strip().rsplit("@", 1)
-        except ValueError:
-            pass
-        else:
             email = email_name.lower() + "@" + domain_part.lower()
+        except (ValueError, AttributeError):
+            # Обработка случаев, когда email не
+            # может быть разделен на имя и домен.
+            # Например, если email равен None или не содержит символа "@".
+            pass
         return email
 
     @classmethod
